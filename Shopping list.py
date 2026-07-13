239f876f36e00e60a0ee6af52351f2835e8919f5
cguargar <87738876+cguargar@users.noreply.github.com>
2026-07-13 15:27:12 +1000

    shopping list test

---
+++ Shopping list.py
@@ -0,0 +1,12 @@
+print("Enter item 1:")
+item1 = input()
+
+print("Enter item 2:")
+item2 = input()
+
+print("Enter item 3:")
+item3 = input()
+
+shopping_list = [item1, item2, item3]
+
+print(shopping_list)
